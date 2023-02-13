994786fd7b41ebbfd0d53760277ed1c5192e3f21
lucidus115 <minecrafter141542@gmail.com>
2023-02-12 23:33:26 -0600

    Set up a basic program with required node modules

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5163
+
+express()
+  .use(express.static(path.join(__dirname, 'resources')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', function (_req, res) {
+    res.render('index', { msg: "Hello World" })
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
