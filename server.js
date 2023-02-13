a3cfb561e5f8cf78f718af3ce61de836ce554bf2
lucidus115 <minecrafter141542@gmail.com>
2023-02-13 00:12:57 -0600

    Implement `/about` route with site info

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ express()
   .use(express.static(path.join(__dirname, 'resources')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/health', function (req, res) {
+  .get('/health', function (_req, res) {
     res.status(200).send("healthy");
   })
   .get('/', function (_req, res) {
     res.render('index', { msg: "Hello World" })
+  })
+  .get('/about', function (_req, res) {
+    res.render('about')
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
