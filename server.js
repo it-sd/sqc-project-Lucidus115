7686dc6320817f14bfeb335452084b1e91f6e289
lucidus115 <minecrafter141542@gmail.com>
2023-02-20 12:44:22 -0600

    Wrap express app in a main function

--- server.js
+++ server.js
@@ -28,32 +28,36 @@ const query = async function (sql) {
   return results
 }
 
-express()
-  .use(express.static(path.join(__dirname, 'resources')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/health', async function (_req, res) {
-    const sql = `SELECT id, username, registered_date
-      FROM user_account
-      LIMIT 1;`
-    
-    const testUser = await query(sql)
+const main = function () {
+  express()
+    .use(express.static(path.join(__dirname, 'resources')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/health', async function (_req, res) {
+      const sql = `SELECT id, username, registered_date
+        FROM user_account
+        LIMIT 1;`
+      
+      const testUser = await query(sql)
 
-    let status = 200
-    let msg = 'healthy'
+      let status = 200
+      let msg = 'healthy'
 
-    // Query failed
-    if (testUser === undefined) {
-      status = 500;
-      msg = 'Failed to query for `TestUser`'
-    }
+      // Query failed
+      if (testUser === undefined) {
+        status = 500;
+        msg = 'Failed to query for `TestUser`'
+      }
 
-    res.status(status).send(msg)
-  })
-  .get('/', function (_req, res) {
-    res.render('index', { msg: 'Hello World' })
-  })
-  .get('/about', function (_req, res) {
-    res.render('about')
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+      res.status(status).send(msg)
+    })
+    .get('/', function (_req, res) {
+      res.render('index', { msg: 'Hello World' })
+    })
+    .get('/about', function (_req, res) {
+      res.render('about')
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+}
+
+main()
