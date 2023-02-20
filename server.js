59e7d4a9be81abcabe393b68d1ae2a14955b66e7
lucidus115 <minecrafter141542@gmail.com>
2023-02-20 11:21:08 -0600

    Have `/health` route query database

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
 require('dotenv').config()
+
+const assert = require('assert')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5163
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
+
+const query = async function (sql) {
+  assert.strictEqual(typeof sql, 'string',
+    'Expected src to be a string')
+  let client
+  let results = []
+  try {
+    client = await pool.connect()
+    const response = await client.query(sql)
+    if (response && response.rows) {
+      results = response.rows
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  if (client) client.release()
+  return results
+}
 
 express()
   .use(express.static(path.join(__dirname, 'resources')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/health', function (_req, res) {
-    res.status(200).send('healthy')
+  .get('/health', async function (_req, res) {
+    const sql = `SELECT id, username, registered_date
+      FROM user
+      LIMIT 1;`
+    
+    const testUser = await query(sql)
+
+    let status = 200
+    let msg = 'healthy'
+
+    // Query failed
+    if (testUser === undefined) {
+      status = 500;
+      msg = 'Failed to query for `TestUser`'
+    }
+
+    res.status(status).send(msg)
   })
   .get('/', function (_req, res) {
     res.render('index', { msg: 'Hello World' })
