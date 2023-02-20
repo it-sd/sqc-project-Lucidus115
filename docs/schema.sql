11e9a273cd748bbacf43d6beadca2dd9b7037044
lucidus115 <minecrafter141542@gmail.com>
2023-02-20 12:29:57 -0600

    Successfully insert user test data

--- docs/schema.sql
+++ docs/schema.sql
@@ -8,18 +8,36 @@ CREATE TABLE user_account (
   username VARCHAR(25) NOT NULL,
   password TEXT NOT NULL,
   email VARCHAR(254) NOT NULL,
-  registered_date TIMESTAMP NOT NULL,
-  projects INT[]
-)
+  registered_date TIMESTAMP WITH TIME ZONE,
+  projects INT[] NULL
+);
 
 CREATE TABLE project (
   id SERIAL PRIMARY KEY,
   title VARCHAR(25) NOT NULL,
-  date_created TIMESTAMP NOT NULL,
-  date_modified TIMESTAMP NOT NULL,
+  date_created TIMESTAMP WITH TIME ZONE,
+  date_modified TIMESTAMP WITH TIME ZONE,
   sound_data TEXT NOT NULL,
   user_id INT NOT NULL
-)
+);
 
-INSERT INTO user (username, password, email, registered_date, projects) VALUES
-  ('TestUser', 'password', 'notareal@email.bruh', 2023-02-20 12:12:12);
+INSERT INTO user_account (username, password, email, registered_date)
+  VALUES
+  (
+    'TestUser', 
+    'password', 
+    'notareal@email.bruh', 
+    '2023-02-20 0:0:0+02'
+  ),
+  (
+    'Autumn', 
+    'thiswillbeencryptedl8r', 
+    'alsonotareal@email.bruh', 
+    '2023-02-20 12:34:56+02'
+  ),  
+  (
+    'Aieee!!', 
+    'qwerty', 
+    'againnotareal@email.bruh', 
+    '2023-02-22 05:06:07-04'
+  );
