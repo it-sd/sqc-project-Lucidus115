d27ac1aab9c1dd5facc7c7b4da5b270f1ec3553d
lucidus115 <minecrafter141542@gmail.com>
2023-03-05 16:44:32 -0600

    Write some sound tests

--- spec/ProjectSpec.js
+++ spec/ProjectSpec.js
@@ -20,3 +20,27 @@ describe('runGatherUsersQuery', function () {
     expect(result.status).toBeLessThanOrEqual(399)
   })
 })
+
+describe('runSoundSearching', function () {
+  const url = 'http://localhost:5163/audio-search'
+
+  const getSound = function (soundId) {
+    return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ soundId }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+  }
+
+  it('should return a knocking soun effect with the id of 677158', async function () {
+    const res = await getSound(677158)
+    const result = await res.json()
+    expect(result.sound.name).toContain('knocking')
+  })
+
+  it('should return null with an invalid id', async function () {
+    const res = await getSound(1)
+    const result = await res.json()
+    expect(result.sound).toBeNull()
+  })
+})
