a7569aa8bfc7791d1b73a2edf7c04703f647f23b
lucidus115 <minecrafter141542@gmail.com>
2023-03-09 02:49:21 -0600

    Create a class representing the audio timeline

--- sound-editor.js
+++ sound-editor.js
@@ -4,6 +4,7 @@ const { default: FreeSound } = require('freesound-client')
 class SoundEditor {
 
   #freeSound
+  #timeline
 
   constructor(freeSoundKey) {
     assert.equal(typeof freeSoundKey, 'string', `Must pass in a string 
@@ -12,18 +13,52 @@ class SoundEditor {
     this.#freeSound = new FreeSound()
     this.#freeSound.setToken(process.env.FREESOUND_KEY)
 
+    this.#timeline = new Timeline()
   }
 
   //TODO: Only show results for those with permissive license
   async searchSounds(text) {
     return this.#freeSound.textSearch(text)
   }
+
+  get timeline() {
+    this.#timeline
+  }
+}
+
+class Timeline {
+  // 2D array of nodes representing rows and columns
+  #nodes
+
+  constructor() {
+    this.#nodes = [[]]
+  }
+
+  insertNode(layer, node) {
+    this.#nodes[layer].push(node)
+  }
+
+  removeNode(layer, node) {
+    const idx = this.#nodes[layer].indexOf(node)
+    this.#nodes[layer].splice(idx, 1)
+  }
+
+  /** Retrives the node in the layer inside given the time (in milliseconds) */
+  getNode(layer, time) {
+    const node = this.#nodes[layer].find(e => e.startTime <= time && e.endTime >= time)
+    return node
+  }
+
+  /** Returns a copy of all the nodes */
+  get nodes() {
+    return this.#nodes.slice(0)
+  }
 }
 
 class Node {
-  layer
   startTime
-  endTime 
+  endTime
+  soundId 
 }
 
 module.exports = {
